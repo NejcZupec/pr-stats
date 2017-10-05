eedc8987167b515fc33875e80ab327900f9a87bc
Peter Lamut <inbox@peterlamut.com>
2017-10-05 15:00:19 +0100

    Add stats aggregator utility class

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from src.data_fetcher import DataFetcher
+from src.stats import StatsAggregator
 from src import serializer
 
 
 def main():
-    fetcher = DataFetcher()
-    prs = fetcher.get_pull_requests()
-    serializer.to_json(prs)
+    # fetcher = DataFetcher()
+    # prs = fetcher.get_pull_requests()
+    # serializer.to_json(prs)
+    aggregator = StatsAggregator()
+    prs_opened_by_day = aggregator.prs_opened_by_day()
+    prs_merged_by_day = aggregator.prs_merged_by_day()
 
 
 if __name__ == '__main__':
